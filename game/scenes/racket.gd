77bb4341cefbb2668eac648d3a8ab3d5aacd389c
zenvoid <zenvoid@a9152a97-29a9-46f2-9092-c0b99d9170f8>
2019-08-15 14:44:05 +0000

    Ignore .import directory
    git-svn-id: https://games.zenvoid.org/dev/MadPong@2 a9152a97-29a9-46f2-9092-c0b99d9170f8

--- game/scenes/racket.gd
+++ game/scenes/racket.gd
@@ -1,4 +1,4 @@
-extends Sprite
+extends KinematicBody2D
 
 var player := 1
 var limit_left: int
@@ -7,11 +7,11 @@ var limit_right: int
 var speed = 100
 
 func set_field_limits(field_left: int, field_right: int):
-	var half_racket_length: int = $Area2D/CollisionShape2D.shape.extents.x
+	var half_racket_length: int = $CollisionShape2D.shape.extents.x
 	limit_left = field_left + half_racket_length + 1
 	limit_right = field_right - half_racket_length - 1
 
-func _process(delta):
+func _physics_process(delta: float):
 	var new_position := position.x
 
 	if Input.is_action_pressed("p%d_left" % player):
