a31ca9e51281be137fd85816560be27d76cb8f7a
rubendesanjuan <rubendesanjuan@a9152a97-29a9-46f2-9092-c0b99d9170f8>
2019-08-22 17:21:14 +0000

    git-svn-id: https://games.zenvoid.org/dev/MadPong@7 a9152a97-29a9-46f2-9092-c0b99d9170f8

--- game/scenes/ball.gd
+++ game/scenes/ball.gd
@@ -6,6 +6,11 @@ var direction := Vector2()
 var limit_right := 0
 var limit_left := 0
 
+var relative_pos : Vector2
+var racket_size : float
+
+var angle : float
+var change_angle
 func _ready():
 	start()
 
@@ -17,9 +22,14 @@ func _physics_process(delta: float):
 
 	var collision := move_and_collide(velocity * delta)
 	if collision:
-		if collision.collider.name.begins_with("Racket"):
+		if ((collision.collider.name.begins_with("Racket")) and (collision.normal.x == 0)):  #Para que no colisione con los laterales de la raqueta y se buguee
+			relative_pos = collision.position - collision.collider.global_position
+			racket_size = collision.collider.get_node("CollisionShape2D").shape.extents.x
+			relative_pos.x += racket_size/2#suma la mitad del ancho para poder sacar la proporción al dividir
 			direction = direction.bounce(Vector2.DOWN)
-			direction.x = 0
-			direction = direction.rotated(rand_range(-PI/3, PI/3)).normalized()
+			direction.x = 0 
+			angle=lerp(-PI/6, PI/6,relative_pos.x/racket_size)
+			change_angle = 1 if collision.collider.name.ends_with("2") else -1
+			direction = direction.rotated(change_angle*angle).normalized()
 		elif collision.collider.name.begins_with("Wall"):
 			direction = direction.bounce(Vector2.RIGHT)
