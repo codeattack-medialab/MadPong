3bd8d01ffcd6229146a22b6f84405a55a9a8d6a7
zenvoid <zenvoid@a9152a97-29a9-46f2-9092-c0b99d9170f8>
2019-09-08 16:33:31 +0000

    Medialab facade mode
    git-svn-id: https://games.zenvoid.org/dev/MadPong@13 a9152a97-29a9-46f2-9092-c0b99d9170f8

--- game/scenes/game.gd
+++ game/scenes/game.gd
@@ -1,7 +1,14 @@
-extends Node
+extends Node2D
+
+export var medialab_facade := false
 
 var score1 := 0
 var score2 := 0
+
+
+func _init():
+	OS.window_fullscreen = true
+
 
 func _ready():
 	$Racket1.player = 1
@@ -12,8 +19,9 @@ func _ready():
 	$Racket1.set_field_limits($Limits/TopLeft.position.x, $Limits/BottomRight.position.x)
 	$Racket2.set_field_limits($Limits/TopLeft.position.x, $Limits/BottomRight.position.x)
 
-#		$"/root/Game".score1+=1
-#		$"/root/Game/Score1".text= str($"/root/Game".score1)
-
-
-
+	if medialab_facade:
+		var viewport_size = Vector2(192+40, 157+40)
+		get_tree().set_screen_stretch(SceneTree.STRETCH_MODE_DISABLED, SceneTree.STRETCH_ASPECT_KEEP, viewport_size, 1)
+		position = Vector2(40, 40)
+	else:
+		position = Vector2(20, 20)
