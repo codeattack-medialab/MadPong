cc58f622948b492efb4dc692a664257190c63e4d
zenvoid <zenvoid@a9152a97-29a9-46f2-9092-c0b99d9170f8>
2019-09-11 12:21:33 +0000

    Move Medialab initialization code
    Place Medialab code in _enter_tree() instead of _ready(), because of a race condition
    
    
    
    git-svn-id: https://games.zenvoid.org/dev/MadPong@20 a9152a97-29a9-46f2-9092-c0b99d9170f8

--- game/scenes/game.gd
+++ game/scenes/game.gd
@@ -6,15 +6,7 @@ var score1 := 0
 var score2 := 0
 
 
-func _ready():
-	$Racket1.player = 1
-	$Racket2.player = 2
-	$Score1.text = "0"
-	$Score2.text = "0"
-
-	$Racket1.set_field_limits($Limits/TopLeft.position.x, $Limits/BottomRight.position.x)
-	$Racket2.set_field_limits($Limits/TopLeft.position.x, $Limits/BottomRight.position.x)
-
+func _enter_tree():
 	if medialab_facade == "FullScreen" or medialab_facade == "Window":
 		var viewport_size = Vector2(192+40, 157+40)
 		get_tree().set_screen_stretch(SceneTree.STRETCH_MODE_DISABLED, SceneTree.STRETCH_ASPECT_KEEP, viewport_size, 1)
@@ -26,6 +18,16 @@ func _ready():
 	elif medialab_facade == "No":
 		OS.window_fullscreen = true
 		position = Vector2(20, 20)
+
+
+func _ready():
+	$Racket1.player = 1
+	$Racket2.player = 2
+	$Score1.text = "0"
+	$Score2.text = "0"
+
+	$Racket1.set_field_limits($Limits/TopLeft.position.x, $Limits/BottomRight.position.x)
+	$Racket2.set_field_limits($Limits/TopLeft.position.x, $Limits/BottomRight.position.x)
 
 
 func _on_body_entered(body,extra_arg_0):
