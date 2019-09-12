c177fa30e067af6b42b2c61342986dca5523b32f
rubendesanjuan <rubendesanjuan@a9152a97-29a9-46f2-9092-c0b99d9170f8>
2019-09-12 12:50:46 +0000

    Falta liberar la escena que sale, está comentado en la linea 68 del script game.gd (está comentado), pero no funciona, hay que cambiarlo. el max_score está a uno, yo pondría al final max_score=3
    git-svn-id: https://games.zenvoid.org/dev/MadPong@21 a9152a97-29a9-46f2-9092-c0b99d9170f8

--- game/scenes/game.gd
+++ game/scenes/game.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
 export(String, "No", "FullScreen", "Window") var medialab_facade := "No"
-
+export var max_score:= 1
 var score1 := 0
 var score2 := 0
-
+var finished := false
 
 func _enter_tree():
 	if medialab_facade == "FullScreen" or medialab_facade == "Window":
@@ -28,7 +28,8 @@ func _ready():
 
 	$Racket1.set_field_limits($Limits/TopLeft.position.x, $Limits/BottomRight.position.x)
 	$Racket2.set_field_limits($Limits/TopLeft.position.x, $Limits/BottomRight.position.x)
-
+	
+	
 
 func _on_body_entered(body,extra_arg_0):
 	$Ball.set_physics_process(false)
@@ -40,5 +41,37 @@ func _on_body_entered(body,extra_arg_0):
 			score2+=1
 			$Score2.text= str(score2)
 	$Ball.position = $Ball.initial_pos
+	if check_finish():
+		finish_game()
+	else:
+		$Timer.start()
+	
+func check_finish() -> bool:
+	if(max(score1,score2)>=max_score):
+		return true
+	else:
+		return false
+		
+func finish_game():
+	var  final_box = preload("res://scenes/final.tscn").instance()
+	self.add_child(final_box)
+	final_box.rect_global_position=Vector2(100,100)
+	$FinalTimer.start()
+	
+func restart():
+	finished=false
+	score1 = 0
+	$Score1.text= str(score1)
+	score2 = 0
+	$Score2.text= str(score2)
 	$Timer.start()
-
+#	self.remove_child("final_box")
+	
+func _final_Timer_timeout():
+	finished=true
+	
+func _process(delta):
+	
+	if (finished and (Input.is_action_just_pressed("p1_left") or Input.is_action_just_pressed("p2_left") or Input.is_action_just_pressed("p1_right") or Input.is_action_just_pressed("p2_right"))):
+		restart()
+		
