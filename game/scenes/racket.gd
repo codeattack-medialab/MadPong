e4380fb81e91ec9559b62afada542486cb81c3a3
rubendesanjuan <rubendesanjuan@a9152a97-29a9-46f2-9092-c0b99d9170f8>
2019-09-19 17:49:06 +0000

    Bugs fixed
    git-svn-id: https://games.zenvoid.org/dev/MadPong@28 a9152a97-29a9-46f2-9092-c0b99d9170f8

--- game/scenes/racket.gd
+++ game/scenes/racket.gd
@@ -8,6 +8,8 @@ var speed = 100
 
 var new_position
 
+var sense = 0
+
 func set_field_limits(field_left: int, field_right: int):
 	var half_racket_length: int = $CollisionShape2D.shape.extents.x
 	limit_left = field_left + half_racket_length + 1
@@ -19,17 +21,17 @@ func _physics_process(delta: float):
 		new_position -= speed * delta
 	if Input.is_action_pressed("p%d_right" % player):
 		new_position += speed * delta
-
+	if sense != 0:
+		new_position  += speed *speed * delta
 	if new_position != position.x:
 		new_position = clamp(new_position, limit_left, limit_right)
 		var movement = new_position - position.x
 		move_and_collide(Vector2(movement, 0))
+		sense = 0
 
 
-func _on_Game_joystick(value,delta):
-	var sense 
+func _on_Game_joystick(value):
 	if value.begins_with("Left-"):
 		sense= -1
 	else:
 		sense= +1
-	new_position += sense * speed * delta
